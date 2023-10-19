29c6913026a5b866655ac70788b7f8eb4f83e0d7
Onur <guresonur@gmail.com>
2023-10-19 16:19:25 +0300

    Update Login.tsx

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -172,7 +172,6 @@ export default class Login extends Component<LoginProps, LoginFormState> {
 
         return (
             <div className="login__control">
-                <img src={dt} alt="login" className="login__dt-logo" width="170px" height="120px" />
                 <p className="login__text">Your tool for Rapid, Reliable & Repeatable deployments</p>
                 {/* @ts-ignore */}
                 <form className="login-dt__form" autoComplete="on" onSubmit={this.login}>
